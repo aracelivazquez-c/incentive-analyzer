734170621605c6fa46a8572409eebee3a58b3eeb
aracelivazquez-c <avazquez_1395@ciencias.unam.mx>
2025-03-23 18:42:59 -0600

    feat: change model

--- analyzer.py
+++ analyzer.py
@@ -58,7 +58,7 @@ Por favor:
 
     with st.spinner("Analizando con IA..."):
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}],
             temperature=0.3
         )
